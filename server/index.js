c967c784fbffecfb2fa980ec0cf3a6e917c9b5b5
Nazarii Lesiuk <nazarii.lesiuk@gopuff.com>
2022-06-21 16:11:06 +0300

    Server - done

--- server/index.js
+++ server/index.js
@@ -2,12 +2,34 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
 const schema = require('./schema');
+const db = {
+  users: [
+    { id: 1, username: 'John Doe', age: 30 },
+    { id: 2, username: 'Meth Be', age: 31 },
+    { id: 3, username: 'Ro nin', age: 32 },
+  ]
+}
+
+const rootResolver = {
+  getAllUsers: () => db.users,
+  getUser: ({ id }) => db.users.find(user => user.id === +id),
+  createUser: ({ input }) => {
+    console.log(input)
+    const user = {
+      id: Date.now(),
+      ...input
+    }
+    db.users.push(user)
+    return user
+  }
+}
 
 const app = express();
 app.use(cors());
 app.use('/graphql', graphqlHTTP({
   graphiql: true,
   schema,
+  rootValue: rootResolver
 }))
 
 app.listen(5000, () => console.log('server app works on port 5000'));
--- server/schema.js
+++ server/schema.js
@@ -1,7 +1,59 @@
 const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
-asd
+
+  type User {
+    id: ID
+    username: String
+    age: Int
+    posts: [Post]
+  }
+  type Post {
+    id: ID
+    title: String
+    content: String
+  }
+
+  input UserInput {
+    id: ID
+    username: String!
+    age: Int!
+    posts: [PostInput]
+  }
+  input PostInput {
+    id: ID
+    title: String!
+    content: String!
+  }
+
+
+  type Query {
+    getAllUsers: [User]
+    getUser(id: ID!): User
+  }
+  type Mutation {
+    createUser(input: UserInput): User
+  }
+
+
 `)
 
-export default schema
+module.exports = schema
+
+/*
+query {
+  getAllUsers {
+    id,
+    username,
+  }
+}
+
+mutation {
+  createUser(input: {
+    username: "Pii2"
+    age: 21
+  }) {
+    id, username,
+  }
+}
+*/
